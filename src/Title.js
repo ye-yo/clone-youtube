7e12ac682fc783c054fd4a14c2ab52c4e80f3beb
ye-yo <yeyo0x0@gmail.com>
2022-01-12 18:05:54 +0900

    Day3: Module CSS 적용

--- src/Title.js
+++ src/Title.js
@@ -1,9 +1,11 @@
+import styles from "./title.module.css";
 function Title() {
+    console.log(styles)
     return (
-        <div>
-            <h1>Netflix</h1>
-            <p>넷플릭스(Netflix)는 대표적인 OTT(over-the-top media service) 스트리밍 플랫폼으로 인터넷을 통해 다양한 미디어 콘텐츠를 제공하는 서비스를 말한다.
-            1997년 Reed Hastings와 Marc Randolph에 의해 설립되었으며 넷플릭스 본사는 미국 캘리포니아에 위치해 있다.
+        <div className={styles['title-box']}>
+            <h1 className={styles.red}>Netflix</h1>
+            <p className={styles.content}>넷플릭스(Netflix)는 대표적인 OTT(over-the-top media service) 스트리밍 플랫폼으로 인터넷을 통해 다양한 미디어 콘텐츠를 제공하는 서비스를 말한다.
+            <br></br>1997년 Reed Hastings와 Marc Randolph에 의해 설립되었으며 넷플릭스 본사는 미국 캘리포니아에 위치해 있다.
             </p>
         </div >
     );
