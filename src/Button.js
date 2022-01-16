5d9ea150c17b337f6256c88091ca5e006272e870
ye-yo <yeyo0x0@gmail.com>
2022-01-16 12:31:12 +0900

    Day5: Props

---
+++ src/Button.js
@@ -0,0 +1,4 @@
+function Button(props) {
+    return <button onClick={props.onClick}>{props.text}</button>;
+}
+export default Button;
---
+++ src/Layout.js
@@ -0,0 +1,7 @@
+function Layout({ children }) {
+    return (
+        <main>{children}</main>
+    );
+}
+
+export default Layout;
