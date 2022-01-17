ae0f70026bbb27cf61c5be01b87bbbeae17395ca
ye-yo <yeyo0x0@gmail.com>
2022-01-17 22:48:32 +0900

    chore: 코드 초기화

--- src/App.js
+++ src/App.js
@@ -1,38 +1,7 @@
-import { useState } from 'react';
-import Layout from './Layout';
-import Button from './Button';
-import styles from "./layout.module.css";
+import './App.css';
 
 function App() {
-  const [value, setValue] = useState(1);
-  function calculate(num, type) {
-    if (type === "sub") {
-      num *= -1;
-    }
-    else if (type === "mul") {
-      num = value;
-    }
-    setValue(value + num);
-  }
-
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  return (
-    <div>
-      <Layout>
-        <h1>구구단 계산기</h1>
-        <div className={styles.table}>
-          {numbers.map((i) => (
-            <p>{value} * {i} = {value * i}</p>
-          ))}
-        </div>
-        <div className={styles['btn-wrap']}>
-          <Button onClick={() => calculate(1, "add")} text="+1" />
-          <Button onClick={() => calculate(1, "sub")} text="-1" />
-          <Button onClick={() => calculate(2, "mul")} text="*2" />
-        </div>
-      </Layout>
-    </div>
-  )
+  return <div>시작</div>;
 }
 
 export default App;
