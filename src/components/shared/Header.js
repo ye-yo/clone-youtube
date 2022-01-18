91f2c32fc0aa12955e955fd1b26989914bdb6fb1
ye-yo <yeyo0x0@gmail.com>
2022-01-18 23:08:29 +0900

    Day 7 - 조건부 렌더링

---
+++ src/components/shared/Header.js
@@ -0,0 +1,7 @@
+import styles from './Header.module.css';
+
+function Header() {
+    return <div className={styles.header}>헤더!</div>;
+}
+
+export default Header;
---
+++ src/components/shared/Menu.js
@@ -0,0 +1,7 @@
+import styles from './Menu.module.css';
+
+function Menu() {
+    return <div className={styles.menu}>메뉴!</div>;
+}
+
+export default Menu;
