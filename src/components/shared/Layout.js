563aa6736663b12df27dacd67de34694a66c3299
ye-yo <yeyo0x0@gmail.com>
2022-01-19 21:32:34 +0900

    Day 8 - 삼항 연산자 활용

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -1,14 +1,21 @@
 import styles from './Layout.module.css';
 import Header from './Header';
 import Menu from './Menu';
+import { useState } from 'react';
+
+
 
 function Layout({ children }) {
+    const [nightMode, setMode] = useState(false)
     return (
-        <div className={styles.container}>
+        <div className={`${styles.container} ${nightMode ? styles.night : styles.day}`}>
             <Header />
             <div className={styles.layout}>
                 <Menu />
-                <div className={styles.contents}>{children}</div>
+                <div className={styles.contents}>
+                    <button className={styles['btn-mode']} onClick={() => setMode(!nightMode)}>{nightMode ? '☀️ Day' : '🌙 Night'} Mode</button>
+                    {children}
+                </div>
             </div>
         </div>
     );
